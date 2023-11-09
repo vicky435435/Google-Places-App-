14e371b530f333deb39fda923359eea1c972e159
Ramkrushna435 <ramkrushna.m@smartlybuilt.com>
2023-11-09 15:53:18 +0530

    html and ts code

--- google-place-app/src/app/home/home.page.ts
+++ google-place-app/src/app/home/home.page.ts
@@ -1,12 +1,53 @@
+import { OnDestroy } from '@angular/core';
+import { NgZone } from '@angular/core';
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+
+declare var google;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit , OnDestroy{
+  
+  places: any[] =[];
+  query: string;
 
-  constructor() {}
+
+  constructor(private zone:NgZone ) {}
+
+ 
+  ngOnInit(): void {
+    
+  }
+
+  async onSearchChange(event:any){
+      console.log(event);
+      this.query = event.details.value;
+      if(this.query.length > 0) await this.getPlaces();
+  }
+
+  async getPlaces(){
+    try{
+      let services = new google.maps.places.AutocompleteService();
+      services.getPlacePredictions({
+        input: this.query,
+      }, (prediction)=>{
+        let autoCompleteItems = [];
+        this.zone.run(()=>{
+          
+        })
+      })
+
+    } catch (e){
+
+    }
+  }
+
+  ngOnDestroy(): void {
+   
+  }
 
 }
